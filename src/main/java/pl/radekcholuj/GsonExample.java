de63b0d3d82b0b6eeb9f384af9df6a59ce49a4fe
Radoslaw Choluj <radekcholuj@wp.pl>
2021-02-28 16:25:38 +0100

    #1 dodanie zależności do Gson, zamiana obiektu na Json

---
+++ src/main/java/pl/radekcholuj/GsonExample.java
@@ -0,0 +1,21 @@
+package pl.radekcholuj;
+
+import com.google.gson.Gson;
+
+public class GsonExample {
+    public static void main(String[] args) {
+        Person p1 = new Person("Jurek", 22);
+        Person p2 = new Person("Marysia", 81);
+
+        System.out.println(p1);
+        System.out.println(p2);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(p1);
+
+        System.out.println(json);
+
+        Person[] people = {p1, p2};
+        System.out.println(gson.toJson(people));
+    }
+}
