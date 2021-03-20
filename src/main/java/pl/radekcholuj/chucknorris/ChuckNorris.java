f4b98257dceacc18da8160a63da483f3052a49f6
Radoslaw Choluj <radekcholuj@wp.pl>
2021-03-20 19:59:33 +0100

    #5 pobranie biblioteki chucknorris, wygenerowanie losowego żartu Chucka Norrisa

---
+++ src/main/java/pl/radekcholuj/chucknorris/ChuckNorris.java
@@ -0,0 +1,16 @@
+package pl.radekcholuj.chucknorris;
+
+import io.chucknorris.client.ChuckNorrisClient;
+import io.chucknorris.client.Joke;
+
+public class ChuckNorris {
+    public static void main(String[] args) {
+
+        // create the chuck norris client
+        ChuckNorrisClient client = new ChuckNorrisClient();
+
+// get a random joke and print it
+        Joke joke = client.getRandomJoke();
+        System.out.println(joke.getValue());
+    }
+}
